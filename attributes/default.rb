b9d6ebfc0bdd6195717c55c3e42708cb66d5dd3a
Tom Howe <turtlebender@gmail.com>
2012-06-20 09:08:19 -0500

    adding sensible defaults to attributes for gearbox.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,5 @@
+default[:gearbox][:apps] = []
 default[:gearbox][:log_dir] = '/var/log/gearbox'
 default[:gearbox][:user] = 'gearbox'
+default[:gearbox][:artifact_bucket] = 'gearbox_build_artifacts'
+default[:gearbox][:app_dir] = '/usr/share/gearbox'

439a953fc0b5a3c1d1c407e13e41ccca76eb0697
Josh Bryan <jbryan@ci.uchicago.edu>
2012-08-08 12:19:41 -0500

    Added license

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
+maintainer       "University of Chicago"
+maintainer_email "jbryan@ci.uchicago.edu"
+license          "http://www.apache.org/licenses/LICENSE-2.0"
 description      "Installs/Configures gearbox"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"

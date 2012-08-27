de76a1089bc7dbf67326f50182fac58ed68e39fc
Tom Howe <turtlebender@gmail.com>
2012-08-27 11:17:48 -0500

    switched to using chef_gem and made globus packages optional

--- recipes/deps.rb
+++ recipes/deps.rb
@@ -13,37 +13,41 @@
 # limitations under the License.
  
 # We need aws-s3 now!
-gem_package "aws-s3" do
+
+chef_gem "aws-s3" do
   action :install
   version "0.6.3"
   options "--no-ri --no-rdoc"
-end.run_action(:install)
-gem_package "mustache" do
+end
+
+chef_gem "mustache" do
   action :install
   version "0.99.4"
   options "--no-ri --no-rdoc"
-end.run_action(:install)
-Gem.clear_paths
+end
+
 directory "/var/lib/globus/downloads" do
   action :create
   recursive true
 end
 
-bash "update_apt" do
-  code <<-eof
-dpkg -i /var/lib/globus/downloads/globus-repository-maverick_0.0.1_all.deb
-apt-get update
-  eof
-  action :nothing
-end
+if node[:enable_globus]
+  bash "update_apt" do
+    code <<-eof
+  dpkg -i /var/lib/globus/downloads/globus-repository-maverick_0.0.1_all.deb
+  apt-get update
+    eof
+    action :nothing
+  end
 
-remote_file "/var/lib/globus/downloads/globus-repository-maverick_0.0.1_all.deb" do
-  action "create_if_missing"
-  source "http://www.globus.org/ftppub/gt5/5.1/5.1.1/installers/repo/globus-repository-maverick_0.0.1_all.deb"
-  checksum "76deac457"
-  notifies :run, "bash[update_apt]", :immediately
-end
+  remote_file "/var/lib/globus/downloads/globus-repository-maverick_0.0.1_all.deb" do
+    action "create_if_missing"
+    source "http://www.globus.org/ftppub/gt5/5.1/5.1.1/installers/repo/globus-repository-maverick_0.0.1_all.deb"
+    checksum "76deac457"
+    notifies :run, "bash[update_apt]", :immediately
+  end
 
-%w{libglobus-gss-assist-dev libglobus-gsi-credential-dev libglobus-gsi-sysconfig-dev libglobus-gsi-cert-utils-dev libglobus-gsi-cert-utils0 libglobus-usage-dev myproxy libxslt1-dev libxml2-dev dictionaries-common wamerican-large}.each do |pkg|
-  package pkg
+  %w{libglobus-gss-assist-dev libglobus-gsi-credential-dev libglobus-gsi-sysconfig-dev libglobus-gsi-cert-utils-dev libglobus-gsi-cert-utils0 libglobus-usage-dev myproxy libxslt1-dev libxml2-dev dictionaries-common wamerican-large}.each do |pkg|
+    package pkg
+  end
 end

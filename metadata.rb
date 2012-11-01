58ffe1714010462a4e5300fc427c4cde7e0ec54c
Tom Howe <turtlebender@gmail.com>
2012-11-01 12:36:43 -0500

    bumping version

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,6 @@ maintainer_email "jbryan@ci.uchicago.edu"
 license          "http://www.apache.org/licenses/LICENSE-2.0"
 description      "Installs/Configures gearbox"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.0.30" 
+version          "0.0.31" 
 depends          "nginx"
+depends          "uwsgi"

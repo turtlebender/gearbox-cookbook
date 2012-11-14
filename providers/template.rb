aea2bb081646ffcc88aba832fa1b3b3014c2f18e
Tom Howe <turtlebender@gmail.com>
2012-11-14 10:14:24 -0600

    fixing frozen path string

--- providers/template.rb
+++ providers/template.rb
@@ -20,57 +20,55 @@ action :create do
     update = ::File.exist?(@new_resource.path)
     context = node.to_hash
     context = context.merge @new_resource.additional_context
-    render_template(new_resource.source, context) do |rendered_template|
-        mode = @new_resource.mode
-        owner = @new_resource.owner
-        group = @new_resource.group
-        target = @new_resource.path
-        file target do
+    mode = @new_resource.mode
+    owner = @new_resource.owner
+    group = @new_resource.group
+    target = @new_resource.path.dup
+    render_template(@new_resource.source, context) do |rendered_template|
+        file @new_resource.path.dup do
             action :nothing
             content rendered_template
             mode mode
             owner owner
             group group
         end.run_action(:create)
+    end
 
-        # Copy upstart files
-        if ( @new_resource.path =~ %r{.*/upstart/.*} )
-            upstart_regex = %r{.*/(.*)\.conf}
-            service_name = upstart_regex.match(@new_resource.path)[1]
-            upstart_config = file "/etc/init/#{service_name}.conf" do
-                content rendered_template
-                action :nothing
-            end
-            upstart_config.run_action(:create)
+    # Copy upstart files
+    if ( @new_resource.path =~ %r{.*/upstart/.*} )
+        upstart_regex = %r{.*/(.*)\.conf}
+        service_name = upstart_regex.match(@new_resource.path)[1]
+        upstart_config = file "/etc/init/#{service_name}.conf" do
+            content rendered_template
+            action :nothing
+        end
+        upstart_config.run_action(:create)
 
-            service = service service_name do
-                provider Chef::Provider::Service::Upstart
-                action :nothing
-            end
-
-            if upstart_config.updated?
-                service.run_action(:enable)
-                service.run_action(:restart)
-            end
+        service = service service_name do
+            provider Chef::Provider::Service::Upstart
+            action :nothing
         end
 
-        if ( @new_resource.path =~ %r{.*/uwsgi/(.*.yaml)} )
-            Chef::Log.info("Matching template: #{@new_resource.path}")
-
-            target_file = ::File.join(node["uwsgi"]["app_path"], $1)
-            FileUtils.mkdir_p(::File.dirname(node["uwsgi"]["app_path"])) unless ::File.exists?(node["uwsgi"]["app_path"]) 
-            Chef::Log.info("Linking source_file #{@new_resource.path} to target_file #{target_file}")
-            source = @new_resource.path
-            link target_file do
-                action :nothing
-                to source
-            end.run_action(:create)
-
-            file source do
-                action :nothing
-            end.run_action(:touch)
+        if upstart_config.updated?
+            service.run_action(:enable)
+            service.run_action(:restart)
         end
     end
+
+    if ( @new_resource.path =~ %r{.*/uwsgi/(.*.yaml)} )
+        Chef::Log.info("Matching template: #{@new_resource.path}")
+
+        target_file = ::File.join(node["uwsgi"]["app_path"], $1)
+        FileUtils.mkdir_p(::File.dirname(node["uwsgi"]["app_path"])) unless ::File.exists?(node["uwsgi"]["app_path"]) 
+        Chef::Log.info("Linking source_file #{@new_resource.path} to target_file #{target_file}")
+        source = @new_resource.path
+        link target_file do
+            action :nothing
+            to source
+        end.run_action(:create)
+
+        file source do
+            action :nothing
+        end.run_action(:touch)
+    end
 end
-
-

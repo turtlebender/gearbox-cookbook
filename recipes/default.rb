5b303edf264ffbcb52ca76a34efc81f13d72e3c6
Nick Allen <nick@nicholasdallen.com>
2012-11-19 15:32:49 -0600

    Place /etc/boto.cfg file

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,18 +3,18 @@
 # Recipe:: default
 #
 # Copyright 1999-2012 University of Chicago
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# 
+#
 
 user "gearbox" do
   system true
@@ -46,3 +46,14 @@ directory node['gearbox']['app_dir'] do
   mode "0775"
 end
 
+template "/etc/boto.cfg" do
+  source "boto.cfg.erb"
+  group "gearbox"
+  owner "gearbox"
+  mode "0644"
+  backup 0
+  variables({
+    :access_key_id => access_key_id,
+    :secret_access_key => secret_access_key
+  })
+end

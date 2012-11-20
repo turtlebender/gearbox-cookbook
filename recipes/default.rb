f4043c50292dd6bbd0dd640d0b00cc6cb873e056
Nick Allen <nick@nicholasdallen.com>
2012-11-20 16:27:37 -0600

    Updated boto key

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,18 +3,18 @@
 # Recipe:: default
 #
 # Copyright 1999-2012 University of Chicago
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# 
+#
 
 user "gearbox" do
   system true
@@ -31,7 +31,7 @@ end
 require "aws/s3"
 require "mustache"
 
-aws_creds = Chef::EncryptedDataBagItem.load("aws_credentials", node["gearbox"]["aws_user"])
+aws_creds = Chef::EncryptedDataBagItem.load("aws_credentials", "application")
 access_key_id = aws_creds["aws_access_key_id"]
 secret_access_key = aws_creds["aws_secret_access_key"]
 

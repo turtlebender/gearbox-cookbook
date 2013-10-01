26ff835330d1d1a18797491a59756cb8fc14483f
Tom Howe <turtlebender@gmail.com>
2013-10-01 07:32:25 -0500

    removing aws-sdk cookbook

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.3.0"
 depends          "nginx"
 depends          "uwsgi"
-depends          "aws_sdk"
 depends          "logrotate"
